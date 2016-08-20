615e877b0398f6d33acb70e875627cf26a053b14
Purwanto07 <purwantoade089@gmail.com>
2016-08-20 10:09:55 +0700

    Add files via upload

---
+++ fungsi_untuk_etollmdr-bri-bni tanggal 040516.sql
@@ -0,0 +1,9 @@
+﻿
+
+--select distinct(etoll_hash) from tbletoll_record where flag = 16 and srlnum like '____0416%' and gerbang ='1' --and shift ='2' and gardu ='06' 
+--GTO 
+--Select * from tbletoll_record_05_2016 where flag = 16 and srlnum like '__040516%' and jam = '07:02:34' --tanggal = '2016-04-30' --and gerbang ='4' and substr(etoll_hash,51,3)= '201'
+select * from tbletoll_record_05_2016 where flag = 16 and srlnum like '__040516%' and jam = '07:02:35' --tanggal = '2016-04-30' --and gerbang ='4' and substr(etoll_hash,51,3)= '201'
+
+--select distinct(etoll_hash)  from tbletoll_record where flag = 16 and srlnum like '__130814%'  and gerbang ='4' and substr(etoll_hash,51,3)= '201'
+
---
+++ fungsi_untuk_etollmdr-bri-bni.sql
@@ -0,0 +1,6 @@
+﻿
+
+--select distinct(etoll_hash) from tbletoll_record where flag = 16 and srlnum like '____0416%' and gerbang ='1' --and shift ='2' and gardu ='06' 
+select * from tbletoll_record_05_2016 where flag = 16 and srlnum like '__020516%' and jam = '20:54:19' --tanggal = '2016-04-30' --and gerbang ='4' and substr(etoll_hash,51,3)= '201'
+--select distinct(etoll_hash)  from tbletoll_record where flag = 16 and srlnum like '__130814%'  and gerbang ='4' and substr(etoll_hash,51,3)= '201'
+
